a5bf222ed935852cd3ae0dece28791527d9c69e2
Saangjun00 <bh0710403@gmail.com>
2025-10-10 17:21:09 +0900

    chore: init

---
+++ backend/src/main/java/com/example/stockmatch/StockmatchApplication.java
@@ -0,0 +1,13 @@
+package com.example.stockmatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockmatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockmatchApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/stockmatch/StockmatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stockmatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockmatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
