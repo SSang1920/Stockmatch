332ad2cfdc6c6d2a419567c0a4b5ece6afbc4bee
Saangjun00 <bh0710403@gmail.com>
2025-10-15 22:24:10 +0900

    feat: ci/cd 워크플로우 통합

--- backend/src/test/java/com/stockmatch/StockmatchApplicationTests.java
+++ backend/src/test/java/com/stockmatch/StockmatchApplicationTests.java
@@ -2,12 +2,16 @@ package com.stockmatch;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @ActiveProfiles("test")
 @SpringBootTest
 class StockmatchApplicationTests {
+
+	@MockitoBean
+	ClientRegistrationRepository clientRegistrationRepository;
 
 	@MockitoBean
 	org.springframework.data.redis.core.RedisTemplate<String, Object> redisTemplate;
