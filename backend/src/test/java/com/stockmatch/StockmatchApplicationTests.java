a351468e90da492944140818f391ce5a67d0fa14
Saangjun00 <bh0710403@gmail.com>
2025-10-15 22:11:57 +0900

    feat: ci/cd 워크플로우 통합

--- backend/src/test/java/com/stockmatch/StockmatchApplicationTests.java
+++ backend/src/test/java/com/stockmatch/StockmatchApplicationTests.java
@@ -3,13 +3,16 @@ package com.stockmatch;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @ActiveProfiles("test")
 @SpringBootTest
 class StockmatchApplicationTests {
 
+	@MockitoBean
+	org.springframework.data.redis.core.RedisTemplate<String, Object> redisTemplate;
+
 	@Test
 	void contextLoads() {
 	}
-
 }
