33fa7b2f9c374d91991bdfb5ac0388026fba918f
Saangjun00 <bh0710403@gmail.com>
2026-05-21 03:28:53 +0900

    fix: 서버 아이피 등록

--- backend/src/main/java/com/stockmatch/config/SecurityConfig.java
+++ backend/src/main/java/com/stockmatch/config/SecurityConfig.java
@@ -75,7 +75,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173")); // Vite React 기본 포트
+        configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://140.245.67.224")); // Vite React 기본 포트
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
